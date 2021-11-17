628857a13118c526ca1c1e338ef127d12d5e2761
Yaroslav Zemlyanskiy <zemlyanskiy.yar@gmail.com>
2021-11-17 06:26:44 +0300

    2урок первый коммит

---
+++ les2.js
@@ -0,0 +1,16 @@
+
+let money = 120000;
+let income = '50000';
+let addExpenses = 'Такси, гей-сауны, интернет, донат в геншин импакт';
+let deposit = true;
+let mission = 600000;
+let period = 6;
+let budgetDay = money/30
+
+console.log(typeof money)
+console.log(typeof income)
+console.log(typeof deposit)
+console.log(addExpenses.length)
+console.log('Период равен ... месяцев')
+console.log(addExpenses.toLowerCase() + addExpenses.split(', '))
+console.log(budgetDay)
